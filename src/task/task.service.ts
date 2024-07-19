9d8058c815dbd602c37234fbab51e610d7c14ca2
BenjaAlegre <capacitaciones06.02@adviters.com>
2024-07-19 12:22:31 -0300

    asasa

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,4 @@
-
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as fs from 'fs/promises';
 import { DATABASE_PATH } from '../constants/globals.constants';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,7 +6,6 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
-
   async create(newTask: CreateTaskDto) {
     const tasks = await this.findAll();
     tasks.push(newTask);
@@ -27,24 +25,21 @@ export class TaskService {
     return task;
   }
 
-  async update(id: number, updateTaskDto: UpdateTaskDto) {
-    const file = JSON.parse(await fs.readFile(DATABASE_PATH).toString());
+  async update(id: string, updateTaskDto: UpdateTaskDto) {
+    const file = await this.findAll();
 
     const index = file.findIndex((task) => task.id == id);
     file[index] = { ...file[index], ...updateTaskDto };
 
     await fs.writeFile(DATABASE_PATH, JSON.stringify(file));
-    return `This action updates a #${id} task`;
   }
 
-  async remove(id: number) {
-    const file = JSON.parse(await fs.readFile(DATABASE_PATH).toString());
+  async remove(id: string) {
+    const file = await this.findAll();
 
     const index = file.findIndex((task) => task.id == id);
     file.splice(index, 1);
 
     await fs.writeFile(DATABASE_PATH, JSON.stringify(file));
-
-    return `This action removes a #${id} task`;
   }
 }
