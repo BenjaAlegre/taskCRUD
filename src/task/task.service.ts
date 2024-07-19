0ed397874cb57db92c8b225450d25925af0fd9e1
lautarolombardi <lombardi.lautaro@outlook.com>
2024-07-19 12:07:41 -0300

    feat: create and red

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,23 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as fs from 'fs/promises';
+import { DATABASE_PATH } from '../constants/globals.constants';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { DATABASE_PATH } from '../constants/globals.constants';
 
 @Injectable()
 export class TaskService {
-
-
-
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  async create(newTask: CreateTaskDto) {
+    const tasks = await this.findAll();
+    tasks.push(newTask);
+    await fs.writeFile(DATABASE_PATH, JSON.stringify(tasks));
   }
 
-  findAll() {
-    return `This action returns all task`;
+  async findAll(): Promise<CreateTaskDto[]> {
+    const data = await fs.readFile(DATABASE_PATH);
+    const tasks: CreateTaskDto[] = JSON.parse(data.toString());
+    return tasks;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: string): Promise<CreateTaskDto> {
+    const tasks = await this.findAll();
+    const task = tasks.find((task) => task.id === id);
+    if (!task) throw new BadRequestException("The task doesn't exist");
+    return task;
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
